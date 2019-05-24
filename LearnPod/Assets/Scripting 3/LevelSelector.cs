d8a784890853613c52a4441c8a8cdcf636c231d9
SpagPix <Spaghettipixel@gmail.com>
2019-05-25 09:59:11 +1000

    A few random scripts

---
+++ LearnPod/Assets/Scripting 3/LevelSelector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int easyDifficulty = 0;
+    public int mediumDifficulty = 1;
+    public int hardDifficulty = 2;
+    public int currentDifficulty;
+   public bool hasMessageSaid;
+   
+   
+
+   
+
+    public void Update()
+    {
+       
+        {
+            if (currentDifficulty == easyDifficulty) //&& hasMessageSaid == false)
+            {
+                Debug.Log("You selected is easy");
+                //hasMessageSaid = true;
+
+            }
+
+            else if (currentDifficulty == mediumDifficulty)// && hasMessageSaid == false)
+            {
+                Debug.Log("You selected medium");
+                //hasMessageSaid = true;
+            }
+            else if (currentDifficulty == hardDifficulty)// && hasMessageSaid == false)
+            {
+                Debug.Log("You selected hard");
+                //hasMessageSaid = true;
+            }
+            else 
+            {
+                Debug.Log("Please select a level before continuing");
+               
+            }
+            //   else if()
+        }
+    }
+}
---
+++ LearnPod/Assets/Scripting 3/PointsCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsCounter : MonoBehaviour
+{
+    [SerializeField]
+    private int _Points;
+    private bool _HasSaidMessage;
+    // create a var collector
+    //everytime hit space generate 10 points
+    // var hasSaidMessage 
+    void Start()
+    {
+
+    }
+
+    // if points counted is greater than 50 print
+    void Update()
+    {
+
+        //if _points is greater or equal to 50 print msg And && or || tell it to stop
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _Points += 10;
+        }
+
+        if (_Points >= 50 && _HasSaidMessage == false)
+        {
+            Debug.Log("Your Amazing");
+            _HasSaidMessage = true;
+        }
+
+    }
+}
